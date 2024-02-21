dd43446e2cace26bbff656b88a2ee7c5d37a39d1
patilniharika2306 <patilniharika2306@gmail.com>
2024-02-21 19:16:22 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MoodifyPage from './components/ModifyPage';
+
+function App() {
+  return (
+   <>
+    <MoodifyPage />
+   </>
+  );
+}
+
+export default App;
